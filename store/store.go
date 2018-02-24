0a20eb54c64bf50823fc62c48ebae8efab6eb96c
Mathieu Lecarme <mlecarme@bearstech.com>
2018-02-24 21:55:37 +0100

    Public

--- store/store.go
+++ store/store.go
@@ -6,25 +6,25 @@ import (
 )
 
 type Store struct {
-	words    *radix.Tree
+	Words    *radix.Tree
 	cpt_word uint32
 	mux      sync.Mutex
 }
 
 func New() *Store {
 	return &Store{
-		words: radix.New(),
+		Words: radix.New(),
 	}
 }
 
 func (s *Store) Word(word []byte) uint32 {
-	k, ok := s.words.Get(word)
+	k, ok := s.Words.Get(word)
 	if ok {
 		return k.(uint32)
 	}
 	s.mux.Lock()
 	defer s.mux.Unlock()
 	s.cpt_word += 1
-	s.words, _, _ = s.words.Insert(word, s.cpt_word)
+	s.Words, _, _ = s.Words.Insert(word, s.cpt_word)
 	return s.cpt_word
 }
