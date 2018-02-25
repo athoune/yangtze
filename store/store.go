3270d6b04d60647fbe849387170487e32232dcc6
Mathieu Lecarme <mlecarme@bearstech.com>
2018-02-25 10:29:50 +0100

    Use an analyzer, not just a tokenizer

--- store/store.go
+++ store/store.go
@@ -1,23 +1,40 @@
 package store
 
 import (
+	"github.com/blevesearch/bleve/analysis"
+	"github.com/blevesearch/bleve/analysis/analyzer/simple"
+	"github.com/blevesearch/bleve/registry"
 	radix "github.com/hashicorp/go-immutable-radix"
 	"sync"
 )
 
 type Store struct {
 	Words    *radix.Tree
+	analyzer *analysis.Analyzer
+	cache    *registry.Cache
 	cpt_word uint32
 	mux      sync.Mutex
 }
 
-func New() *Store {
+func New(analyzer *analysis.Analyzer) *Store {
 	return &Store{
-		Words: radix.New(),
+		Words:    radix.New(),
+		analyzer: analyzer,
+		cache:    registry.NewCache(),
 	}
 }
 
-func (s *Store) Word(word []byte) uint32 {
+func NewSimple() *Store {
+	cache := registry.NewCache()
+	analyzer, _ := simple.AnalyzerConstructor(nil, cache)
+	return &Store{
+		Words:    radix.New(),
+		analyzer: analyzer,
+		cache:    cache,
+	}
+}
+
+func (s *Store) AddWord(word []byte) uint32 {
 	k, ok := s.Words.Get(word)
 	if ok {
 		return k.(uint32)
@@ -27,4 +44,30 @@ func (s *Store) Word(word []byte) uint32 {
 	s.cpt_word += 1
 	s.Words, _, _ = s.Words.Insert(word, s.cpt_word)
 	return s.cpt_word
+}
+
+func (s *Store) Word(word []byte) uint32 {
+	k, ok := s.Words.Get(word)
+	if ok {
+		return k.(uint32)
+	}
+	return 0
+}
+
+func (s *Store) AddSentence(sentence []byte) []uint32 {
+	tokens := s.analyzer.Analyze(sentence)
+	r := make([]uint32, len(tokens))
+	for i, token := range tokens {
+		r[i] = s.AddWord(token.Term)
+	}
+	return r
+}
+
+func (s *Store) Sentence(sentence []byte) []uint32 {
+	tokens := s.analyzer.Analyze(sentence)
+	r := make([]uint32, len(tokens))
+	for i, token := range tokens {
+		r[i] = s.Word(token.Term)
+	}
+	return r
 }
--- store/store_test.go
+++ store/store_test.go
@@ -6,13 +6,15 @@ import (
 )
 
 func TestStore(t *testing.T) {
-	s := New()
-	a1 := s.Word([]byte("Pim"))
-	a2 := s.Word([]byte("Pam"))
-	a3 := s.Word([]byte("Poum"))
-	aa1 := s.Word([]byte("Pim"))
+	s := NewSimple()
+	a1 := s.AddWord([]byte("pim"))
+	a2 := s.AddWord([]byte("pam"))
+	a3 := s.AddWord([]byte("poum"))
+	aa1 := s.AddWord([]byte("pim"))
 	assert.Equal(t, a1, uint32(1))
 	assert.Equal(t, a2, uint32(2))
 	assert.Equal(t, a3, uint32(3))
 	assert.Equal(t, a1, aa1)
+	r := s.Sentence([]byte("Pam, Pim, Poum and the captain"))
+	assert.Equal(t, r, []uint32{2, 1, 3, 0, 0, 0})
 }
