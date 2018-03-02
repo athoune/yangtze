ea8db6d61c52850f5c950da3aa0a7dc851dc782d
Mathieu Lecarme <mlecarme@bearstech.com>
2018-03-02 18:58:56 +0100

    Big cleaning

--- index/index.go
+++ index/index.go
@@ -1,89 +1,35 @@
 package index
 
 import (
-	"bytes"
 	"fmt"
-	"github.com/athoune/yangtze/serialize"
+	"github.com/athoune/yangtze/pattern"
+	"github.com/athoune/yangtze/store"
 	"github.com/blevesearch/bleve/analysis"
-	"github.com/blevesearch/bleve/analysis/tokenizer/whitespace"
-	"github.com/blevesearch/bleve/registry"
-	radix "github.com/hashicorp/go-immutable-radix"
 )
 
 type Index struct {
-	words     *radix.Tree
-	sequences *radix.Tree
-	cpt_word  uint32
-	cpt_seq   uint32
-	tokenizer analysis.Tokenizer
-	cache     *registry.Cache
+	store    *store.Store
+	patterns []*pattern.Pattern
 }
 
-func New() (*Index, error) {
-	cache := registry.NewCache()
-	tokenizer, err := whitespace.TokenizerConstructor(nil, cache)
-	if err != nil {
-		return nil, err
-	}
+func New(analyzer *analysis.Analyzer) (*Index, error) {
 	return &Index{
-		words:     radix.New(),
-		sequences: radix.New(),
-		cache:     cache,
-		tokenizer: tokenizer,
+		store:    store.New(analyzer),
+		patterns: make([]*pattern.Pattern, 0),
 	}, nil
-
 }
 
-/*
-Index a sequence of tokens
-Returns sequence id and potential error
-*/
-func (i *Index) WatchFor(sequence []byte) (uint32, error) {
-	tokens := i.tokenizer.Tokenize(sequence)
-	var seq bytes.Buffer
-	for _, token := range tokens {
-		k, ok := i.words.Get(token.Term)
-		var cpt uint32
-		if ok {
-			cpt = k.(uint32)
-		} else {
-			i.cpt_word += 1
-			cpt = i.cpt_word
-			i.words, _, ok = i.words.Insert(token.Term, cpt)
-		}
-		fmt.Println(token.Term, cpt)
-		seq.Write(serialize.Encode(cpt))
-	}
-	fmt.Println(seq.Bytes())
-	k, ok := i.sequences.Get(seq.Bytes())
-	var ks uint32
-	if ok {
-		ks = k.(uint32)
-	} else {
-		i.cpt_seq += 1
-		ks = i.cpt_seq
-	}
-
-	return ks, nil
+func NewSimple() (*Index, error) {
+	return &Index{
+		store: store.NewSimple(),
+	}, nil
 }
 
-/*
-Transform a sequence of tokens (a line of logs), in a collection of token id, as bytes.
+func (i *Index) AddPattern(p *pattern.Pattern) {
+	i.patterns = append(i.patterns, p)
+}
 
-*/
-func (i *Index) Sequence(line []byte) ([]byte, error) {
-	tokens := i.tokenizer.Tokenize(line)
-	var seq bytes.Buffer
-	for _, token := range tokens {
-		k, ok := i.words.Get(token.Term)
-		var cpt uint32
-		if ok {
-			cpt = k.(uint32)
-		} else {
-			cpt = 0
-		}
-		fmt.Println(token.Term, cpt)
-		seq.Write(serialize.Encode(cpt))
-	}
-	return seq.Bytes(), nil
+func (i *Index) ReadLine(line []byte) {
+	s := i.store.Sentence(line)
+	fmt.Println(s)
 }
--- index/index_test.go
+++ index/index_test.go
@@ -6,18 +6,9 @@ import (
 )
 
 func TestWatchFor(t *testing.T) {
-	i, err := New()
+	i, err := NewSimple()
 	if err != nil {
 		t.Error(err)
 	}
-	_, err = i.WatchFor([]byte("pim pam poum pim"))
-	if err != nil {
-		t.Error(err)
-	}
-
-	l, err := i.Sequence([]byte("Aunt pim and the captain"))
-	if err != nil {
-		t.Error(err)
-	}
-	fmt.Println(l)
+	fmt.Println(i)
 }
