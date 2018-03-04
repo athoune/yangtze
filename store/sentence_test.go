81c16b52788e93b6260e0c0a2cbf9b03fec8b0ed
Mathieu Lecarme <mlecarme@bearstech.com>
2018-03-04 17:03:08 +0100

    test bad things

--- store/sentence_test.go
+++ store/sentence_test.go
@@ -9,4 +9,7 @@ func TestSentence(t *testing.T) {
 	s1 := Sentence{1, 2, 3, 4, 5}
 	s2 := Sentence{3, 4}
 	assert.Equal(t, 2, s1.Index(s2))
+	s3 := Sentence{5, 6}
+	assert.Equal(t, -1, s1.Index(s3))
+	assert.Equal(t, -1, s2.Index(s1))
 }
