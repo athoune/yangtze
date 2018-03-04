67608403907421daed216f350ccc5f728d5eafa8
Mathieu Lecarme <mlecarme@bearstech.com>
2018-03-04 17:09:46 +0100

    barebone

--- store/sentence.go
+++ store/sentence.go
@@ -25,10 +25,8 @@ func (s Sentence) Index(substr Sentence) int {
 		return -1
 	}
 	i2 := 0
-	var w2 Word
 	for i1, w1 := range s {
-		w2 = substr[i2]
-		if w1 == w2 {
+		if w1 == substr[i2] {
 			i2 += 1
 			if i2 == len(substr) {
 				return i1 - len(substr) + 1
