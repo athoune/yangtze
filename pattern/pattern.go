bcb26db9de4e1649541affbfc641bce955c62e9d
Mathieu Lecarme <mlecarme@bearstech.com>
2018-03-04 20:22:42 +0100

    Token store a Sentence, not just a Word

--- pattern/pattern.go
+++ pattern/pattern.go
@@ -12,6 +12,7 @@ type Kind int
 const (
 	JustAToken Kind = iota
 	Star            // .
+	Optional        // ?
 	AllStars        // ...
 )
 
@@ -38,7 +39,7 @@ type Token struct {
 	Kind       Kind
 	Position   int
 	StartsWith bool // *
-	Word       store.Word
+	Sentence   store.Sentence
 }
 
 func NewToken(value string, position int) *Token {
@@ -46,51 +47,48 @@ func NewToken(value string, position int) *Token {
 	if s {
 		value = value[0 : len(value)-1]
 	}
-	var k Kind
-	switch value {
-	case ".":
-		k = Star
-	case "...":
-		k = AllStars
-	default:
-		k = JustAToken
-	}
 	return &Token{
-		Kind:       k,
+		Kind:       whatKind(value),
 		Value:      value,
 		Position:   position,
 		StartsWith: s,
+		Sentence:   make(store.Sentence, 0),
+	}
+}
+
+func whatKind(value string) Kind {
+	switch value {
+	case ".":
+		return Star
+	case "?":
+		return Optional
+	case "...":
+		return AllStars
+	default:
+		return JustAToken
 	}
 }
 
 func (p *Parser) Parse(src string) (*Pattern, error) {
 	tokens := p.tokenizer.Tokenize([]byte(src))
 	s := Pattern{
-		Tokens:        make([]*Token, len(tokens)),
+		Tokens:        make([]*Token, 0),
 		HasStartsWith: false,
 	}
-	for i, tok := range tokens {
+	var previous *Token = nil
+	for _, tok := range tokens {
 		t := NewToken(string(tok.Term), tok.Start)
-		if t.Kind == JustAToken && p.store != nil {
-			t.Word = p.store.AddWord([]byte(t.Value))
+		if previous == nil || t.Kind != JustAToken || (t.Kind == JustAToken && previous.Kind != JustAToken) {
+			s.Tokens = append(s.Tokens, t)
+			previous = t
 		}
-		s.Tokens[i] = t
+		if t.Kind == JustAToken {
+			previous.Sentence = append(previous.Sentence, p.store.AddWord(tok.Term))
+		}
 		s.HasStartsWith = s.HasStartsWith || t.StartsWith
 	}
 
 	return &s, nil
-}
-
-func (p *Pattern) Sentence() store.Sentence {
-	sentences := make(store.Sentence, len(p.Tokens))
-	for i, t := range p.Tokens {
-		if t.Kind == JustAToken {
-			sentences[i] = t.Word
-		} else {
-			sentences[i] = 0
-		}
-	}
-	return sentences
 }
 
 func (p *Pattern) Match(sentence store.Sentence) bool {
