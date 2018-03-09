331f05fd4803deb927292c9b55d511d12aa1d0f5
Mathieu Lecarme <mlecarme@bearstech.com>
2018-03-09 14:22:33 +0100

    Test bitset stuff

--- store/sentence_test.go
+++ store/sentence_test.go
@@ -7,7 +7,10 @@ import (
 
 func TestSentence(t *testing.T) {
 	s1 := NewSentence(1, 2, 3, 4, 5)
+	assert.Equal(t, 5, s1.Length())
+	assert.Equal(t, uint(5), s1.Bitset.Count())
 	s2 := NewSentence(3, 4)
+	assert.Equal(t, uint(2), s2.Bitset.Count())
 	assert.Equal(t, 2, s1.Index(s2))
 	s3 := NewSentence(5, 6)
 	assert.Equal(t, -1, s1.Index(s3))
